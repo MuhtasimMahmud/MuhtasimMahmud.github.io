befe48837894add95ada309d9903a53bb253391c
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2024-07-12 17:30:22 +0600

    updated

--- forms/contact.php
+++ forms/contact.php
@@ -1,42 +1,17 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve form data
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $subject = htmlspecialchars($_POST['subject']);
-    $message = htmlspecialchars($_POST['message']);
+if($_POST["submit"]) {
+    
+    $recipient="muhtasim.mzs16@gmail.com"; 
 
-    // Validate email
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "Invalid email format";
-        exit;
-    }
+    $subject= $_POST["subject"]; 
+    $sender=$_POST["name"];
+    $senderEmail=$_POST["email"];
+    $message=$_POST["message"];
 
-    // Set the recipient email address
-    $to = 'muhtasim.mzs16@gmail.com';
+    $mailBody="Name: $sender\nEmail Address: $senderEmail\n\nMessage: $message";
 
-    // Set the email subject
-    $email_subject = "Contact Form: $subject";
-
-    // Build the email content
-    $email_body = "You have received a new message from the contact form on your website.\n\n".
-                  "Here are the details:\n".
-                  "Name: $name\n".
-                  "Email: $email\n".
-                  "Subject: $subject\n\n".
-                  "Message:\n$message";
-
-    // Set the email headers
-    $headers = "From: $email\n";
-    $headers .= "Reply-To: $email";
-
-    // Send the email
-    if (mail($to, $email_subject, $email_body, $headers)) {
-        echo "Your message has been sent. Thank you!";
-    } else {
-        echo "There was an error sending your message. Please try again.";
-    }
-} else {
-    echo "Invalid request method";
+    mail($recipient, $subject, $mailBody);
+    sleep(1);
+    // header("Location:http://blog.antonyraphel.in/sample/"); // Set here redirect page or destination page
 }
 ?>
